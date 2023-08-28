7e5c5c4620f208841efea1fb6ea0867ed4415601
Zach Barnes <16zacharybarnes@gmail.com>
2023-08-28 18:23:20 -0500

    deleted a folder and fixed URL routing. Application now runs

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,7 @@
 const db = require('express').Router();
 
 // GET route for retrieving all notes
-db.get('/', (req, res) => readFromFile('../develop/db/db.json').then((data) => res.json(JSON.parse(data))));
+db.get('/', (req, res) => readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data))));
 
 // POST route for submitting notes
 db.post('/', (req, res) => {
